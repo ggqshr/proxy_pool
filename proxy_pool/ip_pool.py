c11e69cfc56c3e1284b056e9de86d0037b551109
ggqshr <942490944@qq.com>
2020-01-07 17:08:01 +0800

    当请求频率过快时，增加睡眠的时间

--- proxy_pool/ip_pool.py
+++ proxy_pool/ip_pool.py
@@ -25,7 +25,7 @@ class IpPool(object):
         res = self._request_ip()
         while res != REQUEST_SUCCESS:
             if res == REQUEST_TOO_QUICK:
-                sleep(5)
+                sleep(10)
                 res = self._request_ip()
                 continue
             if res == REQUEST_REACH_MAX:
@@ -77,6 +77,7 @@ class IpPool(object):
             res = self._request_ip()
             while res != REQUEST_SUCCESS:
                 if res == REQUEST_TOO_QUICK:
+                    sleep(10)
                     res = self._request_ip()
                     continue
                 if res == REQUEST_REACH_MAX:
