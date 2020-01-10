b33086f74e8193a18c8e7c6dbf1286a848d29d8e
ggqshr <942490944@qq.com>
2020-01-10 10:47:25 +0800

    增加日志部分

--- proxy_pool/data5u_proxy.py
+++ proxy_pool/data5u_proxy.py
@@ -2,23 +2,31 @@ import json
 import logging
 from random import choice
 from time import sleep
-
+import yaml
 import requests
-
+from os.path import join, dirname
 from proxy_pool import IpPool
 import threading
+import logging.config
+
+with open(join(dirname(__file__), "logconf.yaml"), "r") as f:
+    logconfig = yaml.load(f, Loader=yaml.FullLoader)  # 加载日志配置
+logging.config.dictConfig(logconfig)  # 设置日志
+logger = logging.getLogger("data5u")
 
 
 class Data5UProxy(IpPool):
-    def __init__(self, api_url):
+    def __init__(self, api_url,enable_log=True):
         super().__init__(api_url)
+        if not enable_log:
+            logger.setLevel(logging.INFO)
 
     def start(self):
         GetIpThread(self.api_url, self.ip_pool, self.cond).start()
         pass
 
     def _request_ip(self):
-        logging.info("请求新的ip")
+        logger.info("请求新的ip")
         res = requests.get(self.api_url).content.decode()
         res = json.loads(res)
         if res['success']:
@@ -27,7 +35,7 @@ class Data5UProxy(IpPool):
                 self.ip_pool.add(f"{dd['ip']}:{dd['port']}")
                 with self.cond:
                     self.cond.notify_all()
-                logging.info("请求成功")
+                logger.info("请求成功")
 
     def get_ip(self):
         with self.cond:
@@ -35,9 +43,9 @@ class Data5UProxy(IpPool):
             return choice(list(self.ip_pool))
 
     def report_baned_ip(self, ip):
-        logging.info(f"remove {ip} from pool!")
+        logger.info(f"remove {ip} from pool!")
         self.ip_pool.discard(ip)
-        logging.info(f"now the pool is {self.ip_pool}")
+        logger.info(f"now the pool is {self.ip_pool}")
 
     def report_bad_net_ip(self, ip):
         pass
@@ -52,14 +60,14 @@ class GetIpThread(threading.Thread):
 
     def run(self) -> None:
         while True:
-            logging.debug("刷新新的ip")
+            logger.debug("刷新新的ip")
             res = requests.get(self.url).content.decode()
             res = json.loads(res)
             if res['success']:
                 all_data = res['data']
                 for dd in all_data:
                     self.ip_pool.add(f"{dd['ip']}:{dd['port']}")
-                    logging.debug("请求成功")
+                    logger.debug("请求成功")
                     with self.cond:
                         self.cond.notify_all()
             sleep(5)
